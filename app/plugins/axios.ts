283bf0eaebd473a8ff7f50d8b828b3bc494159cd
segzyswitch <segzyswitch99@gmail.com>
2025-07-30 14:57:40 +0100

    first comm

--- app/plugins/axios.ts
+++ app/plugins/axios.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
   const api = axios.create({
-    baseURL: config.public.apiUrl.replace(/\/$/, ''), // clean trailing slash
+    // baseURL: config.public.apiUrl.replace(/\/$/, ''), // clean trailing slash
   })
 
   nuxtApp.provide('axios', api)
--- app/services/api.ts
+++ app/services/api.ts
@@ -1,6 +1,9 @@
-// import axios from 'axios';
-
-const { $axios } = useNuxtApp();
+import axios from 'axios';
+const apiUrl = process.env.API_URL || 'http://localhost/coinma/api';
+// const { $axios } = useNuxtApp();
+const $axios = axios.create({
+  // baseURL: config.public.apiUrl.replace(/\/$/, ''), // clean trailing slash
+});
 
 class Request {
   static getCSC() {
@@ -13,15 +16,19 @@ class Request {
 
   // User
   static Register(fd: any) {
-    return $axios.post('/register', fd);
+    return $axios.post(`${apiUrl}/register`, fd);
   }
   static Login(fd: object) {
-    const Forms = JSON.stringify(fd);
-    return $axios.post('/login', Forms);
+    // const Forms = JSON.stringify(fd);
+    return $axios.post(`${apiUrl}/login`, fd);
   }
   static Logout() {
     const ACCESS_TOKEN = useCookie('auth_token').value;
-    return $axios.post('/logout');
+    return $axios.post('/logout', {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    });
   }
   static userDetails() {
     const ACCESS_TOKEN = useCookie('auth_token').value;
