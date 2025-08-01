1c2e87f00e3061cfadd3b09c70e8f8e95642b9ad
segzyswitch <segzyswitch99@gmail.com>
2025-08-01 16:42:40 +0100

    ssr-false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  // ssr: false,
+  ssr: false,
   modules: ['@pinia/nuxt'],
   vite: {
     server: {
